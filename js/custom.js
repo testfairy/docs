deaf4e9c01767587fa48e8eaefe8b084c484833e
gmegidish <gil@testfairy.com>
2017-11-02 13:19:07 +0200

    Restored opening folders

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,9 @@
 $(function () {
-	/* gilm
     $('.aj-nav').click(function (e) {
         e.preventDefault();
         $(this).parent().siblings().find('ul').slideUp();
         $(this).next().slideToggle();
     });
-	*/
 
     // Bootstrap Table Class
     $('table').addClass('table');
